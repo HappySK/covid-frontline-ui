478e49a9af925db66aa63c45a4f84fce7e575853
HappySK <shravanyoserene1729@gmail.com>
2021-06-02 02:05:47 +0530

    Improved Form Validations and included forgot password link

--- src/Volunteers/login_volunteers.js
+++ src/Volunteers/login_volunteers.js
@@ -1,107 +1,118 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, Route, useParams, Redirect, useHistory } from "react-router-dom";
+import { useFormik, Field, ErrorMessage, Formik, Form } from "formik";
+import { Button } from "@material-ui/core";
+import * as Yup from "yup";
 
 function VolunteerLogin() {
-  const [User, setUser] = useState({
-    email: "",
-    password: "",
-  });
+	const history = useHistory();
 
-  const history = useHistory();
+	const initialValues = {
+		email: "",
+		password: "",
+	};
 
-  const { email, password } = User;
+	const validationSchema = Yup.object({
+		email: Yup.string()
+			.email("Please provide valid email format")
+			.required("Email is required"),
+		password: Yup.string().required("Password is Required"),
+	});
 
-  const handleChange = (email) => (event) => {
-    setUser({ ...User, [email]: event.target.value });
-  };
+	const onSubmit = (values, actions) => {
+		axios
+			.post(
+				`${process.env.REACT_APP_BASE_URL}/volunteers/volunteerslogin`,
+				values
+			)
+			.then((res) => {
+				console.log(res);
+				sessionStorage.setItem("user", JSON.stringify(res.data));
+				history.push("/dashboard");
+			})
+			.catch((err) => {
+				if (err) actions.setFieldError("password", "Invalid Credentials");
+				actions.setSubmitting(false);
+			});
+	};
 
-  console.log(email, password);
+	const formik = useFormik({
+		initialValues,
+		validationSchema,
+		onSubmit,
+	});
 
-  const Lo = () => {
-    axios
-      .post("https://api.covidfrontline.net/volunteers/volunteerslogin", {
-        email: email,
-        password: password,
-      })
-      .then((res) => {
-        console.log(res);
-
-        sessionStorage.setItem("user", JSON.stringify(res.data));
-
-        history.push("/volunteer_request");
-      });
-
-    // setUser({
-    //   isLoggedin : true,
-    //   googleID : response.googleId,
-    //   name : response.profileObj.name,
-    //   email : response.profileObj.email,
-    //   accesstoken : response.accessToken,
-    //   picture : response.profileObj.imageUrl,
-    //   didredirect : true
-    // })
-  };
-
-  return (
-    <div>
-      <div className="h-100 w-100 bg-color">
-        <div className="login-box">
-          <div className="login-logo">
-            {/* <span>
-              <strong style={{ fontSize: "20px" }}>
-                COVID HELP - Volunteer
-              </strong>
-            </span> */}
-           <img src="/assets/img/covid_logo.png" />
-            {/* <img src="assets/img/logo/deepthoughtlogo.svg" alt="COVID HELP"/> */}
-          </div>
-          <form className="form-contact contact_form">
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    onChange={handleChange("email")}
-                    value={email}
-                    name="email"
-                    type="text"
-                    onfocus="this.placeholder = ''"
-                    onblur="this.placeholder = 'Username'"
-                    placeholder="Email*"
-                  />
-                </div>
-              </div>
-              <div className="col-sm-12">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    type="password"
-                    onChange={handleChange("password")}
-                    value={password}
-                    name="password"
-                    onfocus="this.placeholder = ''"
-                    onblur="this.placeholder = '******'"
-                    placeholder="******"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="form-group mt-3 mb-0">
-              <div className="w-100 btn  boxed-btn" onClick={Lo()}>
-                Login
-              </div>
-
-              <p>
-                If you want to login as Admin ?{" "}
-                <Link to="/adminlogin">Admin Login</Link>
-              </p>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="h-100 w-100 bg-color">
+				<div className="login-box">
+					<div className="login-logo">
+						<span>
+							<strong style={{ fontSize: "20px" }}>
+								COVID HELP - VOLUNTEER
+							</strong>
+						</span>
+						<img src="/assets/img/covid_logo.png" />
+						{/* <img src="assets/img/logo/deepthoughtlogo.svg" alt="COVID HELP"/> */}
+					</div>
+					<Formik
+						initialValues={initialValues}
+						onSubmit={onSubmit}
+						validationSchema={validationSchema}
+					>
+						{(formik) => (
+							<Form>
+								<div className="row">
+									<div className="col-sm-12">
+										<div className="form-group">
+											<Field
+												type="email"
+												className="form-control"
+												name="email"
+												placeholder="Enter email"
+											/>
+											<ErrorMessage name="email" />
+										</div>
+									</div>
+									<div className="col-sm-12">
+										<div className="form-group">
+											<Field
+												type="password"
+												className="form-control"
+												name="password"
+												placeholder="Enter password"
+											/>
+											<ErrorMessage name="password" />
+										</div>
+									</div>
+								</div>
+								<div className="form-group mt-3 mb-0">
+									<div className="w-100 btn  boxed-btn">
+										<Button
+											type="submit"
+											className="btn boxed-btn"
+											fullWidth
+											diabled={!formik.isSubmitting}
+										>
+											{formik.isSubmitting ? `Loading` : `Submit`}
+										</Button>
+									</div>
+									<p>
+										<Link to="/change_password">Forgot Password ?</Link>
+									</p>
+									<p>
+										If you want to login as Admin ?{" "}
+										<Link to="/adminlogin">Admin Login</Link>
+									</p>
+								</div>
+							</Form>
+						)}
+					</Formik>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default VolunteerLogin;
