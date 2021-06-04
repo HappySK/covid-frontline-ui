b8f068af67c2964f3ccbbdfb3600c3c2cbf1d38f
HappySK <shravanyoserene1729@gmail.com>
2021-06-04 10:45:07 +0530

    Pagination feature in Institution details

--- src/components/InstitutionDetails/InstitutionDetails.js
+++ src/components/InstitutionDetails/InstitutionDetails.js
@@ -1,6 +1,6 @@
 import React, { useMemo, useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { useTable } from "react-table";
+import { useTable, usePagination } from "react-table";
 import Loader from "react-loader-spinner";
 import swal from "sweetalert";
 
@@ -110,10 +110,27 @@ export const InstitutionDetails = () => {
 	const columns = useMemo(() => COLUMNS, []);
 	const data = useMemo(() => institutions);
 
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-		useTable({ columns, data });
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		rows,
+		prepareRow,
+		page,
+		previousPage,
+		nextPage,
+		canPreviousPage,
+		canNextPage,
+		goToPage,
+		state,
+		setPageSize,
+	} = useTable(
+		{ columns, data, initialState: { pageIndex: 0, pageSize: 1 } },
+		usePagination
+	);
 
 	const [loading, setLoading] = useState(true);
+	const { pageIndex, pageSize } = state;
 
 	return (
 		<div>
@@ -129,48 +146,79 @@ export const InstitutionDetails = () => {
 								</button>
 							</Link>
 						</div>
-						<div className="table-responsive admin-table">
-							{loading ? (
-								<div style={{ marginLeft: "500px", marginTop: "200px" }}>
-									{" "}
-									<Loader
-										type="Circles"
-										color="#f39510"
-										height={100}
-										width={100}
-										timeout={3000} //3 secs
-									/>
-								</div>
-							) : (
-								<table {...getTableProps()}>
-									<thead>
-										{headerGroups.map((headerGroup) => (
-											<tr>
-												{headerGroup.headers.map((column) => (
-													<th {...column.getHeaderProps()}>
-														{column.render("Header")}
-													</th>
-												))}
-											</tr>
-										))}
-									</thead>
-									<tbody {...getTableBodyProps()}>
-										{rows.map((row) => {
-											prepareRow(row);
-											return (
-												<tr {...row.getRowProps()}>
-													{row.cells.map((cell) => (
-														<td {...cell.getCellProps()}>
-															{cell.render("Cell")}
-														</td>
+						{loading ? (
+							<div style={{ marginLeft: "500px", marginTop: "200px" }}>
+								{" "}
+								<Loader
+									type="Circles"
+									color="#f39510"
+									height={100}
+									width={100}
+									timeout={3000} //3 secs
+								/>
+							</div>
+						) : (
+							<>
+								<div className="table-responsive admin-table">
+									<table {...getTableProps()}>
+										<thead>
+											{headerGroups.map((headerGroup) => (
+												<tr>
+													{headerGroup.headers.map((column) => (
+														<th {...column.getHeaderProps()}>
+															{column.render("Header")}
+														</th>
 													))}
 												</tr>
-											);
-										})}
-									</tbody>
-								</table>
-							)}
-						</div>
+											))}
+										</thead>
+										<tbody {...getTableBodyProps()}>
+											{page.map((row) => {
+												prepareRow(row);
+												return (
+													<tr {...row.getRowProps()}>
+														{row.cells.map((cell) => (
+															<td {...cell.getCellProps()}>
+																{cell.render("Cell")}
+															</td>
+														))}
+													</tr>
+												);
+											})}
+										</tbody>
+									</table>
+								</div>
+								<div className="d-flex justify-content-between p-3">
+									<select
+										value={pageSize}
+										onChange={(e) => setPageSize(Number(e.target.value))}
+										className="form-control form-control-sm w-25 align-self-center"
+									>
+										{[1, 2, 3].map((pageSize) => (
+											<option key={pageSize} value={pageSize}>
+												Show {pageSize}
+											</option>
+										))}
+									</select>
+									<div>
+										<button
+											className="button button-contactForm boxed-btn"
+											onClick={() => previousPage()}
+											disabled={!canPreviousPage}
+										>
+											Previous
+										</button>
+										<button
+											className="button button-contactForm boxed-btn"
+											onClick={() => nextPage()}
+											disabled={!canNextPage}
+										>
+											Next
+										</button>
+									</div>
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 			</div>
